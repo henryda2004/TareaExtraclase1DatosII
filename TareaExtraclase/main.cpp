f34a20c9b80bdde446c2c08485817ecdcdc26934
henryda2004 <henryda2004@gmail.com>
2023-03-07 11:02:21 -0600

    Creacion de la clase Node y List. Se crea el metodo de insercion.

--- TareaExtraclase/main.cpp
+++ TareaExtraclase/main.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
 
+class Node{
+public:
+    int valor;
+    Node *next;
+    Node (int valor){
+        this->valor = valor;
+        this->next = NULL;
+    }
+
+};
+
+class List{
+private:
+    void insertar_inicio(Node *&cabeza, int valor){
+        Node *nuevo_nodo = new Node(valor);
+
+        Node *aux = cabeza;
+
+        cabeza = nuevo_nodo;
+        nuevo_nodo->next = aux;
+    }
+public:
+    Node *inicio;
+
+    void insertar_nuevo(int valor){
+        if (this->inicio = NULL){
+            this->inicio = new Node(valor);
+        } else{
+            this->insertar_inicio(inicio, valor);
+        }
+    }
+
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    List *list  = new List();
+    list->insertar_nuevo(8);
+    list->insertar_nuevo(9);
+
     return 0;
 }
