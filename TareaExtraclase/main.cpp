2e4f05a443e22b54e87f690868359178b2323e12
henryda2004 <henryda2004@gmail.com>
2023-03-09 10:57:54 -0600

    Clase collector lista y overload funcional, se debe arreglar la clase lista para el overload

--- TareaExtraclase/main.cpp
+++ TareaExtraclase/main.cpp
@@ -16,10 +16,23 @@ public:
         valor = nuevo_valor;
         std::cout<<"Valor modificado correctamente" << std::endl;
     }
-    void obtener_valor(){
+    int obtener_valor(){
         std::cout<<"EL valor almacenado es: " << valor <<std::endl;
+        return valor;
     }
+    void * operator new(size_t size)
+    {
+        std::cout<< "Overloading new operator with size: " << size << std::endl;
+        void * p = ::operator new(size);
+        //void * p = malloc(size); will also work fine
 
+        return p;
+    }
+    void operator delete(void * p)
+    {
+        std::cout<< "Overloading delete operator " << std::endl;
+        free(p);
+    }
 };
 
 class List{
@@ -52,14 +65,14 @@ private:
             }
             else if(anterior == NULL){
                 cabeza = cabeza->next;
-                delete aux_eliminar;
+                delete(aux_eliminar);
                 std::cout<<"Valor a eliminar encontrado" << std::endl;
 
             }
             else{
                 std::cout<<"Valor a eliminar encontrado" << std::endl;
                 anterior->next = aux_eliminar->next;
-                delete aux_eliminar;
+                delete(aux_eliminar);
             }
         }
     }
@@ -97,21 +110,62 @@ public:
 
 class Collector{
 private:
+    void insertar_collector_private(Node *&cabeza, int valor){
+        Node *nuevo_nodo = new Node(valor);
+
+        Node *aux = cabeza;
+
+        cabeza = nuevo_nodo;
+        cabeza->next = aux;
+    }
+    void eliminar_nodo_collector(Node *&cabeza){
+        if(cabeza != NULL){
+            Node *aux_eliminar;
+
+            aux_eliminar = cabeza;
+
+
+            cabeza = cabeza->next;
+            delete(aux_eliminar);
+            std::cout<<"Primer nodo de Collector eliminado correctamente" << std::endl;
+
+        }
+    }
 public:
+    Node *inicio = NULL;
+    void insertar_collector(int valor){
+
+        insertar_collector_private(inicio, valor);
+    }
+    bool validar_existencia(){
+        if (inicio != NULL){
+            std::cout<<"Collector con nodo(s) restantes" << std::endl;
+            return true;
+        } else{
+            std::cout<<"Collector vacio" << std::endl;
+            return false;
+        }
+    }
+    int obtener_valor (){
+        int x = inicio->obtener_valor();
+        eliminar_nodo_collector(inicio);
+        std::cout<<"El valor obtenido de collector es: " << x <<std::endl;
+
+        return x;
+    }
 };
 
 int main() {
 
-    List *list  = new List();
-    list->insertar_nuevo(8);
-    list->insertar_nuevo(9);
-    list->insertar_nuevo(7);
-    list->insertar_nuevo(10);
-    list->mostrar_lista();
-    list->asignar_valor_cabeza(11);
-    list->mostrar_lista();
-    list->eliminar_nodo(7);
-    list->mostrar_lista();
+    Node *ptr = new Node(4);
+    Collector *collector = new Collector();
+    collector->insertar_collector(3);
+    collector->insertar_collector(32);
+    collector->validar_existencia();
+    collector->obtener_valor();
+    collector->validar_existencia();
+    ptr->obtener_valor();
+    delete ptr;
 
 
     return 0;
